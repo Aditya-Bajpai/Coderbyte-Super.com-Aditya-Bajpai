c5a714a96d6961fde82a1a5b7db6e8923488e32e
Aditya Bajpai <75834500+Aditya-Bajpai@users.noreply.github.com>
2025-09-22 13:15:36 -0400

    Update coderbyte.py

--- coderbyte.py
+++ coderbyte.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 #stringio was also a viable alternative to write the data into the csv file, however i could not recall it's implementation at the time of coding this :(
 # so i went ahead using the open function 
+#this version fills in missing flight codes while preserving existing values, while the first submission overwrote the entire column in new sequence
 
 
 data = 'Airline Code;DelayTimes;FlightCodes;To_From\nAir Canada (!);[21, 40];20015.0;WAterLoo_NEWYork\n<Air France> (12);[];;Montreal_TORONTO\n(Porter Airways. );[60, 22, 87];20035.0;CALgary_Ottawa\n12. Air France;[78, 66];;Ottawa_VANcouvER\n""".\\.Lufthansa.\\.""";[12, 33];20055.0;london_MONTreal\n'
@@ -28,6 +29,7 @@ df[["From", "To"]] = df["To_From"].str.upper().str.split("_", expand=True)
 df = df.drop(columns=["To_From"])
 
 df.to_csv("final_flight_data.csv", index=False)
+
 
 
 
